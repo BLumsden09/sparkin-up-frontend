46c8a6d5a8ac20ebaadc06775ed302231940749a
Brandon <bpl5@students.uwf.edu>
2018-02-08 04:28:40 -0500

    Created/ designed landing and sign in pages. TODO:  Create and add routes to register, password recovery, and tour pages. Then, link sign in, register, and password recovery pages to backend.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Test123
+# Sparkin' Up
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.7.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { SigninComponent } from './components/signin/signin.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SigninComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
